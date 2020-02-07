978d84937dcc29cc98109269344c54b9c81e5398
esraaamal <ma.esraa@hotmail.com>
2020-02-07 13:59:26 +0200

    rg

---
+++ caal.js
@@ -0,0 +1,24 @@
+
+
+function greatGreating(){
+    var hourNow;
+    hourNow=prompt('what is time now? 0-24');
+    var name ;
+    name= prompt('what is your name');
+    var greeting;
+    if (hourNow>0 && hourNow<=12)
+    {greeting='Good morning!';}
+    else if (hourNow>12 && hourNow<=18)
+    {greeting='Good afternon!';}
+    else if (hourNow>18 && hourNow<=24)
+    {greeting='Good night';}
+    else{greeting = 'welcome';
+    }
+    
+    document.write('<h3>' + greeting + '  ' + name + '</h3>' )
+    }
+    greatGreating();
+    
+    
+
+
--- css/caal.js
+++
@@ -1,8 +0,0 @@
-
-prompt("enter your name");
-prompt("enter your email");
-prompt("enter your age");
-confirm("press enter to continue");
-alert("don't skip that");
-    
-
